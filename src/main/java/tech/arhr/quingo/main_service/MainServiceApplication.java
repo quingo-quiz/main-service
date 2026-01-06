a144afa3836dbca841f5fc62f5e80156f580a221
artem961 <artemkhrabrov03@gmail.com>
2026-01-07 01:27:41 +0300

    init repository

---
+++ src/main/java/tech/arhr/quingo/main_service/MainServiceApplication.java
@@ -0,0 +1,13 @@
+package tech.arhr.quingo.main_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainServiceApplication.class, args);
+	}
+
+}
