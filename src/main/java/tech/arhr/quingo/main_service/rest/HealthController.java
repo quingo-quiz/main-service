28fee81b8645aef8aad10e9ae6f2787f5b72419c
artem961 <artemkhrabrov03@gmail.com>
2026-01-08 00:14:14 +0300

    testing workflow

--- src/main/java/tech/arhr/quingo/main_service/rest/HealthController.java
+++ src/main/java/tech/arhr/quingo/main_service/rest/HealthController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
     @GetMapping("/health")
     public String health() {
-        return "health";
+        return "service healthy";
     }
 }
