31e299fb3496ae7088579b731edc197b0cd241a2
artem961 <artemkhrabrov03@gmail.com>
2026-01-07 17:53:41 +0300

    add hot reload

--- src/main/java/tech/arhr/quingo/main_service/rest/HealthController.java
+++ src/main/java/tech/arhr/quingo/main_service/rest/HealthController.java
@@ -2,12 +2,13 @@ package tech.arhr.quingo.main_service.rest;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
     @GetMapping("/health")
-    public ResponseEntity<?> test() {
-        return ResponseEntity.ok().build();
+    public String health() {
+        return "health";
     }
 }
